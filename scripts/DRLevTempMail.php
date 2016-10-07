8190b4406a6f2aca558aa088e1fc9fa9425580e5
dr_Lev <drlev52@gmail.com>
2016-10-07 17:40:34 +0300

    add mail class

---
+++ scripts/DRLevTempMail.php
@@ -0,0 +1,38 @@
+<?php
+
+require_once __DIR__.'/DRLevScript.php';
+
+class DRLevTempMail extends DRLevScript {
+    protected $email;
+    protected $iterationCount = 120; // count of check iterations
+    protected $iterationWait = 0.5; // time in seconds to wait before next iteration
+    protected $iterationNo = 0;
+
+    public function start() {
+        $this->iterationNo++;
+        if (empty($this->email)) {
+            throw new Exception("Email is empty");
+        }
+
+        $md5 = md5($this->email);
+        $data = json_encode(file_get_contents("http://api.temp-mail.ru/request/mail/id/{$md5}/format/json"), true);
+        if (isset($data['error'])) {
+            if ($this->iterationNo <= $this->iterationCount) {
+                usleep($this->iterationWait * 1000000);
+                $this->start();
+            } else {
+                throw new Exception('Failure to get approve email');
+            }
+        } else {
+            var_dump(array_keys($data[0]));
+        }
+    }
+
+    public function setEmail($email) {
+        $this->email = $email;
+    }
+
+    public function getEmail() {
+        return $this->email;
+    }
+}
